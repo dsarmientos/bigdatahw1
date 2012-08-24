c23039e440c5a6eab6603148439ffb3074781f35
Daniel Sarmiento <dsarmientos@gmail.com>
2012-08-23 22:07:25 -0500

    Query usando contains no funciona

--- src/rss/views.py
+++ src/rss/views.py
@@ -17,17 +17,8 @@ def home(request):
     parsed_feeds = get_parsed_feeds()
     for feed in parsed_feeds:
         titles.extend(
-            map(lambda n: '%s' % n['title'],
-                feed['items']))
+        ['%s' % item['title'] for item in feed['items']])
     return render_to_response('index.html', {'titulos': titles})
-
-
-def get_parsed_feeds():
-    feeds_xml = get_feeds_xml()
-    parsed_feeds = []
-    for feed_xml in feeds_xml:
-        parsed_feeds.append(feedparser.parse(feed_xml))
-    return parsed_feeds
 
 
 def filtro_regex(request):
@@ -54,46 +45,58 @@ def filtro_regex(request):
 def filtro_xquery(request):
     if request.method != 'GET' or 'q'not in request.GET:
         return HttpResponseBadRequest()
-    zorba, store, dataManager, docManager = init_zorba()
+    zorba, store, data_manager, doc_manager = init_zorba()
     keyword = request.GET['q'].encode('ascii', 'xmlcharrefreplace').lower()
     xquery = build_xquery(zorba, keyword)
     results = []
     for xml in get_feeds_xml():
-        docManager.put("rss.xml", build_doc(dataManager, xml))
+        doc_manager.put("rss.xml", build_doc(data_manager, xml))
         results.append(xquery.execute())
-        docManager.remove("rss.xml")
+        doc_manager.remove("rss.xml")
     shutdown_zorba(zorba, store)
     return HttpResponse(results, mimetype='text/html')
+
+def get_parsed_feeds():
+    feeds_xml = get_feeds_xml()
+    parsed_feeds = []
+    for feed_xml in feeds_xml:
+        parsed_feeds.append(feedparser.parse(feed_xml))
+    return parsed_feeds
 
 
 def init_zorba():
     store = zorba_api.InMemoryStore_getInstance()
     zorba = zorba_api.Zorba_getInstance(store)
-    dataManager = zorba.getXmlDataManager()
-    docManager = dataManager.getDocumentManager()
-    return zorba, store, dataManager, docManager
+    data_manager = zorba.getXmlDataManager()
+    doc_manager = data_manager.getDocumentManager()
+    return zorba, store, data_manager, doc_manager
 
 
 def build_xquery(zorba, keyword):
     # contains($item/category, "%(kw)s") does not work: more than one category
-    query = r'for $item in doc("rss.xml")//item ' \
-            'let $title := lower-case($item/title) ' \
-            'where contains($title, "%(kw)s") or ' \
-            'contains($item/description, "%(kw)s") ' \
-            'return <tr><td>{data($item/title)}</td>' \
-            '<td>{data($item/pubDate)}</td><td>' \
-            '<a href="{data($item/link)}">Ver</a>' \
-            '</td></tr>' % {'kw': keyword}
+    query = (
+        'for $item in doc("rss.xml")//item '
+        'let $title := lower-case($item/title) '
+        'let $description := lower-case($item/description) '
+        'where contains($title, "%(kw)s") or '
+          'contains($description, "%(kw)s") or '
+          'some $category in $item/category '
+            'satisfies contains($category, lower-case("%(kw)s")) '
+        'return <tr>'
+          '<td>{data($item/title)}</td>'
+          '<td>{data($item/pubDate)}</td>'
+          '<td><a href="{data($item/link)}">Ver</a></td>'
+        '</tr>') % {'kw': keyword}
     return zorba.compileQuery(query)
 
 
-def build_doc(dataManager, xml):
-    docIter = dataManager.parseXML(xml)
-    docIter.open()
+def build_doc(data_manager, xml):
+    doc_iter = data_manager.parseXML(xml)
+    doc_iter.open()
     doc = zorba_api.Item_createEmptyItem()
-    docIter.next(doc)
-    docIter.close()
-    docIter.destroy()
+    doc_iter.next(doc)
+    doc_iter.close()
+    doc_iter.destroy()
     return doc
 
 
